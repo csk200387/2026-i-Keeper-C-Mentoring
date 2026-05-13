2150929d217d16e9bed9ddad1f6224d1ef48515b
csk200387 <102679480+csk200387@users.noreply.github.com>
2026-05-13 10:28:41 +0000

    5주차

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+// 컴파일 및 실행 명령어 : gcc main.c -o main ; ./main
 #include <stdio.h>
 
-int main() {
-    printf("%c\n", 49);
+
+int fib(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    printf("피보나치 %d 번째 항은 %d 입니다.\n", n, fib(n));
 }
